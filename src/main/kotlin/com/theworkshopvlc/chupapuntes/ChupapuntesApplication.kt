4b116f4b141f60c91f3ba4ad2cf0570585014f39
Cotel <miguelcoletomunoz@hotmail.com>
2018-04-22 19:23:21 +0200

    First commit with basic project

---
+++ src/main/kotlin/com/theworkshopvlc/chupapuntes/ChupapuntesApplication.kt
@@ -0,0 +1,11 @@
+package com.theworkshopvlc.chupapuntes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChupapuntesApplication
+
+fun main(args: Array<String>) {
+  runApplication<ChupapuntesApplication>(*args)
+}
