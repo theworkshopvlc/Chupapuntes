6af409aab5cfe67de682aa036556e2575dcbbfa2
diphendara <cristiancvacas@gmail.com>
2018-04-29 14:15:42 +0200

    Merge branch 'categories' of https://github.com/theworkshopvlc/Chupapuntes into categories
    # Conflicts:
    #	src/main/kotlin/com/theworkshopvlc/chupapuntes/categories/CategoriesController.kt

--- src/test/kotlin/com/theworkshopvlc/chupapuntes/categories/domain/CategoryGetByTitleTest.kt
+++ src/test/kotlin/com/theworkshopvlc/chupapuntes/categories/domain/CategoryGetByTitleTest.kt
@@ -1,6 +1,29 @@
 package com.theworkshopvlc.chupapuntes.categories.domain
 
-class CategoryGetByTitleTest {
+import com.theworkshopvlc.chupapuntes.categories.persistence.ICategoriesDAO
 
+import org.junit.Test
+import org.mockito.Mockito
+
+class CategoryGetByTitleTest {
+  var mockedDAO = Mockito.mock(ICategoriesDAO::class.java)
+
+  @Test
+  fun `if no exist categories with that text in the title should return empty list`() {
+    Mockito.`when`(mockedDAO.findByTitle("Test")).thenReturn(emptyList())
+  }
+
+  @Test
+  fun `if text is Maths should return 2 categories`() {
+    Mockito.`when`(mockedDAO.findByTitle("Maths")).thenReturn(multipleResult())
+  }
+
+  @Test
+  fun `if text is Simple should return 1 category`() {
+    Mockito.`when`(mockedDAO.findByTitle("Simple")).thenReturn(singleReturn())
+  }
+
+  fun multipleResult(): List<Category> = listOf(Category(0, "Maths"), Category(0, "Simple Maths"))
+  fun singleReturn(): List<Category> = listOf(Category(0, "Simple Maths"))
 
 }
