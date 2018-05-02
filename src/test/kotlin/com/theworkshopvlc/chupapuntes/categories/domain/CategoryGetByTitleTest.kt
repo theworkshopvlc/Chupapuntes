fa9cbea0b73325f48940e7810278945e55ecc01a
diphendara <cristiancvacas@gmail.com>
2018-05-02 20:39:29 +0200

    Added asserts to the tests

--- src/test/kotlin/com/theworkshopvlc/chupapuntes/categories/domain/CategoryGetByTitleTest.kt
+++ src/test/kotlin/com/theworkshopvlc/chupapuntes/categories/domain/CategoryGetByTitleTest.kt
@@ -10,17 +10,27 @@ class CategoryGetByTitleTest {
 
   @Test
   fun `if no exist categories with that text in the title should return empty list`() {
-    Mockito.`when`(mockedDAO.findByTitle("Test")).thenReturn(emptyList())
+    `when`(mockedDAO.findByTitle("Test")).thenReturn(emptyList())
+    val resultList: List<Category> = mockedDAO.findByTitle("Programming")
+
+    assertThat(resultList.length).isEqualsTo(0)
   }
 
   @Test
   fun `if text is Maths should return 2 categories`() {
-    Mockito.`when`(mockedDAO.findByTitle("Maths")).thenReturn(multipleResult())
+    `when`(mockedDAO.findByTitle("Maths")).thenReturn(multipleResult())
+    val resultList: List<Category> = mockedDAO.findByTitle("Maths")
+
+    assertThat(resultList.length).isEqualsTo(2)
   }
 
   @Test
   fun `if text is Simple should return 1 category`() {
-    Mockito.`when`(mockedDAO.findByTitle("Simple")).thenReturn(singleReturn())
+    `when`(mockedDAO.findByTitle("Simple")).thenReturn(singleReturn())
+
+    val resultList: List<Category> = mockedDAO.findByTitle("Simple")
+
+    assertThat(resultList.length).isEqualsTo(1)
   }
 
   fun multipleResult(): List<Category> = listOf(Category(0, "Maths"), Category(0, "Simple Maths"))
