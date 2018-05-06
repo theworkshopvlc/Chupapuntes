558504673969d24cfd0f202af535aea944035681
Cotel <miguelcoletomunoz@hotmail.com>
2018-05-06 17:25:06 +0200

    Returning QuestionResponse

--- src/main/kotlin/com/theworkshopvlc/chupapuntes/questions/QuestionController.kt
+++ src/main/kotlin/com/theworkshopvlc/chupapuntes/questions/QuestionController.kt
@@ -3,6 +3,8 @@ package com.theworkshopvlc.chupapuntes.questions
 import com.theworkshopvlc.chupapuntes.questions.model.errors.QuestionValidationsResults
 import com.theworkshopvlc.chupapuntes.questions.model.entities.Question
 import com.theworkshopvlc.chupapuntes.questions.model.entities.QuestionRequest
+import com.theworkshopvlc.chupapuntes.questions.model.entities.QuestionResponse
+import com.theworkshopvlc.chupapuntes.questions.model.entities.toResponse
 import com.theworkshopvlc.chupapuntes.questions.model.usecases.CreateQuestion
 import com.theworkshopvlc.chupapuntes.questions.model.usecases.GetAllQuestions
 import org.springframework.http.HttpStatus
@@ -19,15 +21,21 @@ class QuestionController(private val getAllQuestions: GetAllQuestions,
                          private val createQuestion: CreateQuestion) {
 
   @GetMapping
-  fun index(): List<Question> =
+  fun index(): List<QuestionResponse> =
     getAllQuestions.execute()
+      .map(Question::toResponse)
 
   @PostMapping
   fun newQuestion(@RequestBody question: QuestionRequest): ResponseEntity<*> {
     val result = createQuestion.execute(question)
     return when (result) {
-      is QuestionValidationsResults.Success -> ResponseEntity.status(HttpStatus.CREATED).body(result.question)
-      else -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result.error)
+      is QuestionValidationsResults.Success -> ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(result.question.toResponse())
+
+      else -> ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(result.error)
     }
   }
 }
